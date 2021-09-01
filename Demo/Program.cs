3b35a992eb3ecdc09a018ecda54e40b26da8cd0d
Bhagyashree Tipare <48119099+BhagyashreeT001@users.noreply.github.com>
2021-09-01 09:28:44 +0530

    Add files via upload

---
+++ Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter any number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                fact *= i;
+            }
+
+            Console.WriteLine("Factorial of " + number + " is: " + fact);
+
+            Console.Read();
+        }
+    }
+}
