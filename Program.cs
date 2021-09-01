2b429138a0a0bd3c0969f69653c3a37a27a9223a
Bhagyashree Tipare <48119099+BhagyashreeT001@users.noreply.github.com>
2021-09-01 22:36:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Q1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write(Math.Round(12.5));
+            Console.WriteLine(Math.Round(10.5));
+            Console.ReadLine();
+        }
+    }
+}
