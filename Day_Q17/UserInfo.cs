4141ca34b9127ee9f7004206fc36499266ae11d5
Bhagyashree Tipare <48119099+BhagyashreeT001@users.noreply.github.com>
2021-09-13 17:51:27 +0530

    Add files via upload

---
+++ Day_Q17/UserInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace Day_Q17
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+        public int Password { get; set; }
+        public string Email { get; set; }
+
+        public UserInfo(string email, string username, int password)
+        {
+            Email = email;
+            UserName = username;
+            Password = password;
+
+        }
+
+
+    }
+}
